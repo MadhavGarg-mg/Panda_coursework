d393a3fc9de13e9077f21565cff570134006aa75
Madhav Garg <c2064676@newcastle.ac.uk>
2022-10-04 11:32:50 +0100

    Add files

--- walking_panda/cli.py
+++ walking_panda/cli.py
@@ -4,11 +4,13 @@ import argparse
 
 def cli():
     parser = argparse.ArgumentParser(prog="walking_panda")
-    parser.add_argument("--no-rotate", help="Suppress Rotation", action="store_true")
-    parser.add_argument("--scale", default=1, help="Scale Panda", type=float)
-    parser.add_argument("--move-side", default=0, help="Move Panda left(positive) or right(negative)", type=float)
-    parser.add_argument("--move", default=0, help="Move Panda forward", type=float)
-    parser.add_argument("--fly", default=0, help="Make the Panda fly", type=float)
+    parser.add_argument("--no-rotate", help="Suppress Rotation.", action="store_true")
+    parser.add_argument("--scale", default=1, help="Scale Panda.", type=float)
+    parser.add_argument("--move-side", default=0, help="Move Panda left(positive) or right(negative).", type=float)
+    parser.add_argument("--move", default=0, help="Move Panda forward(positive) or behind(negative).", type=float)
+    parser.add_argument("--fly", default=0, help="Make the Panda fly.", type=float)
+    # parser.add_argument("--rotate-speed", default=0, help="Make the camera rotate faster.", type=int)
+
     args = parser.parse_args()
 
     walking = panda.WalkingPanda(**vars(args))
--- walking_panda/panda.py
+++ walking_panda/panda.py
@@ -3,12 +3,11 @@ from math import pi, sin, cos
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
+
 import simpleaudio as sa
-from time import time, sleep
 
 wave_obj = sa.WaveObject.from_wave_file("panda_power.wav")
 play_obj = wave_obj.play()
-# play_obj.wait_done()
 
 
 class WalkingPanda(ShowBase):
@@ -22,10 +21,11 @@ class WalkingPanda(ShowBase):
         self.scene.reparentTo(self.render)
         # Apply scale and position transforms on the model.
         self.scene.setScale(0.25, 0.25, 0.25)
-        self.scene.setPos(-5 + move_side, 42 + move, 0 - fly)
+        self.scene.setPos(-8 + move_side, 42 + move, 0 - fly)
 
         # Add the spinCameraTask procedure to the task manager.
         self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+
         # Load and transform the panda actor.
         self.pandaActor = Actor("models/panda-model",
                                 {"walk": "models/panda-walk4"})
@@ -36,7 +36,7 @@ class WalkingPanda(ShowBase):
 
     # Define a procedure to move the camera.
     def spinCameraTask(self, task):
-        if self.no_rotate == False:
+        if not self.no_rotate:
             angleDegrees = task.time * 6.0
             angleRadians = angleDegrees * (pi / 180.0)
         else:
