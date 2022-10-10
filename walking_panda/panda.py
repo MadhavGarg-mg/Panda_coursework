a8191910b2b5b2d6a379ba89fa051628673d93c8
Madhav Garg <c2064676@newcastle.ac.uk>
2022-10-10 11:22:50 +0100

    Added hexagon panda to the file.

--- walking_panda/panda.py
+++ walking_panda/panda.py
@@ -4,6 +4,9 @@ from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
 
+from direct.interval.IntervalGlobal import Sequence
+from panda3d.core import Point3
+
 import simpleaudio as sa
 
 wave_obj = sa.WaveObject.from_wave_file("panda_power.wav")
@@ -37,6 +40,52 @@ class WalkingPanda(ShowBase):
         # Loop its animation.
         if not self.no_walk:
             self.pandaActor.loop("walk")
+
+        posInterval1 = self.pandaActor.posInterval(1,
+                                                   Point3(1.9, 3, 0),
+                                                   startPos=Point3(3.5, 0, 0))
+        posInterval2 = self.pandaActor.posInterval(1,
+                                                   Point3(-1.9, 3, 0),
+                                                   startPos=Point3(1.9, 3, 0))
+        posInterval3 = self.pandaActor.posInterval(1,
+                                                   Point3(-3.5, 0, 0),
+                                                   startPos=Point3(-1.9, 3, 0))
+        posInterval4 = self.pandaActor.posInterval(1,
+                                                   Point3(-1.9, -3, 0),
+                                                   startPos=Point3(-3.5, 0, 0))
+        posInterval5 = self.pandaActor.posInterval(1,
+                                                   Point3(1.9, -3, 0),
+                                                   startPos=Point3(-1.9, -3, 0))
+        posInterval6 = self.pandaActor.posInterval(1,
+                                                   Point3(3.5, 0, 0),
+                                                   startPos=Point3(1.9, -3, 0))
+        hprInterval1 = self.pandaActor.hprInterval(1,
+                                                   Point3(-60, 0, 0),
+                                                   startHpr=Point3(0, 0, 0))
+        hprInterval2 = self.pandaActor.hprInterval(1,
+                                                   Point3(-120, 0, 0),
+                                                   startHpr=Point3(-60, 0, 0))
+        hprInterval3 = self.pandaActor.hprInterval(1,
+                                                   Point3(-180, 0, 0),
+                                                   startHpr=Point3(-120, 0, 0))
+        hprInterval4 = self.pandaActor.hprInterval(1,
+                                                   Point3(-240, 0, 0),
+                                                   startHpr=Point3(-180, 0, 0))
+        hprInterval5 = self.pandaActor.hprInterval(1,
+                                                   Point3(-300, 0, 0),
+                                                   startHpr=Point3(-240, 0, 0))
+        hprInterval6 = self.pandaActor.hprInterval(1,
+                                                   Point3(-360, 0, 0),
+                                                   startHpr=Point3(-300, 0, 0))
+        # Create and play the sequence that coordinates the intervals.
+        self.pandaPace = Sequence(posInterval1, hprInterval1,
+                                  posInterval2, hprInterval2,
+                                  posInterval3, hprInterval3,
+                                  posInterval4, hprInterval4,
+                                  posInterval5, hprInterval5,
+                                  posInterval6, hprInterval6,
+                                  name="pandaPace")
+        self.pandaPace.loop()
 
     # Define a procedure to move the camera.
     def spinCameraTask(self, task):
