e373369458e0ac15b82a753176f90cfa65cc7295
Madhav Garg <c2064676@newcastle.ac.uk>
2022-10-10 10:14:50 +0100

    Added --no-walk argument to the file.

--- walking_panda/cli.py
+++ walking_panda/cli.py
@@ -4,6 +4,7 @@ import argparse
 
 def cli():
     parser = argparse.ArgumentParser(prog="walking_panda")
+
     parser.add_argument("--no-rotate", help="Suppress Rotation.", action="store_true")
     parser.add_argument("--scale", default=1, help="Scale Panda.", type=float)
     parser.add_argument("--move-side", default=0, help="Move Panda left(positive) or right(negative).", type=int)
@@ -11,6 +12,7 @@ def cli():
     parser.add_argument("--fly", default=0, help="Make the Panda fly.", type=int)
     parser.add_argument("--rotate-speed", default=1, help="Make the camera rotate faster.", type=float)
     parser.add_argument("--zoom", default=1, help="Make the camera zoom in or out.", type=float)
+    parser.add_argument("--no-walk", help="Makes the panda stop walking.", action="store_true")
 
     args = parser.parse_args()
 
